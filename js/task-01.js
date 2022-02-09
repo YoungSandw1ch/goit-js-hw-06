58f2726f24ce16b4e619782ce64296a6311f18af
YoungSandw1ch <sersus1991@gmail.com>
2022-02-09 16:31:00 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,13 @@
 const items = document.querySelectorAll('.item');
-
 console.log(`Number of categories: `, items.length);
 
 items.forEach(item => {
-  console.log(`Category: ${item.querySelector('h2').textContent}`
-  `Elements: `, item.querySelectorAll('li').length)
-  // console.log(`Elements: `, item.querySelectorAll('li').length);
+  console.log(`Category: ${item.querySelector('h2').textContent}
+Elements: ${item.querySelectorAll('li').length}`)
 });
+
+//======или так если нужно чтоб количество элементов было числом
+// items.forEach(item => {
+//   console.log(`Category: ${item.querySelector('h2').textContent}`);
+//   console.log(`Elements: `, item.querySelectorAll('li').length);
+// });
