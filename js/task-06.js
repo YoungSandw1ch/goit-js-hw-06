ad98ddeb2293dfe9cd43c033ee11563102e3c7d6
YoungSandw1ch <sersus1991@gmail.com>
2022-02-10 21:48:38 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -11,12 +11,15 @@ function onValidationInputBlur(event) {
     : validationInputRef.setAttribute('class', 'invalid');
 }
 
-//====отображатор длины введенной строки для удобства
+/*
+*====отображатор длины введенной строки для удобства========
+*/
 
 console.log('Счетчик добавлен динамически, HTML не изменял');
 
 let value = 0;
 const title = document.createElement('h1');
+title.style.fontSize = '16px';
 
 validationInputRef.after(title);
 
@@ -26,3 +29,17 @@ function onValidationInput(event) {
   value = event.currentTarget.value.length;
   title.textContent = `Вы ввели ${value} символов`;
 };
+
+/*
+*==автоматически меняет цвет бордера в зависимости от длины==
+*/
+
+// validationInputRef.addEventListener('input', onInputChangeBorder);
+
+// function onInputChangeBorder(event) {
+//   const length = event.currentTarget.value.length;
+//   const rightLength = Number(validationInputRef.dataset.length);
+//   validationInputRef.style.outline = 'transparent';
+
+//   validationInputRef.style.borderColor = length !== rightLength ? 'red' : 'green';
+// };
