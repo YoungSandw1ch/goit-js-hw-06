34410753b5fdfb61590ab1a3e232f5367522293f
Sergei Mikhailenko <sersus1991@gmail.com>
2022-02-11 11:13:23 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const refs = {
+  body: document.querySelector('body'),
+  colorValue: document.querySelector('.color'),
+  changeColorBtn: document.querySelector('.change-color'),
+}
+
+refs.changeColorBtn.addEventListener('click', changeBodyBgColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeBodyBgColor() {
+  const color = getRandomHexColor();
+
+  refs.body.style.backgroundColor = color;
+  refs.colorValue.textContent = color;
 }
