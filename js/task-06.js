f27d093c8cf153e5d8e58c3ea5354b9f78de5452
YoungSandw1ch <sersus1991@gmail.com>
2022-02-12 00:04:00 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -6,10 +6,12 @@ function onValidationInputBlur(event) {
   const valueLength = event.currentTarget.value.length;
   const dataLength = Number(validationInputRef.dataset.length);
   
-  valueLength === dataLength
-    ? validationInputRef.setAttribute('class', 'valid')
-    : validationInputRef.setAttribute('class', 'invalid');
-}
+  if (valueLength === dataLength) {
+    validationInputRef.setAttribute('class', 'valid');
+  } else {
+    validationInputRef.setAttribute('class', 'invalid');
+  }
+};
 
 /*
 *====отображатор длины введенной строки для удобства========
